6106ceae93a5a7b1f68600b33d086aec97113238
Victor Hugo <vhsds2000@gmail.com>
2023-01-24 21:24:55 -0300

    finalization of the project

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,27 @@
 const btnMobile = document.getElementById('btn-mobile');
+const btnTrailer = document.querySelector('.btn');
+const btnCloseModal = document.querySelector('.close-modal');
+const video = document.getElementById('video');
+const modal = document.querySelector('.modal');
+const linkVideo = video.src;
 
 function toggleMenu() {
     const nav = document.getElementById('nav');
     nav.classList.toggle('active');
 }
 
-btnMobile.addEventListener('click',toggleMenu)
+function alternarModal() {
+    modal.classList.toggle("open");
+}
+
+btnMobile.addEventListener('click',toggleMenu);
+
+btnTrailer.addEventListener('click', () => {
+    alternarModal();
+    video.setAttribute("src", linkVideo);
+});
+
+btnCloseModal.addEventListener('click', () => {
+    alternarModal();
+    video.setAttribute("src", "");
+});
